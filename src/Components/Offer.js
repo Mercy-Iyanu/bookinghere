b4ddba729cf38b35861c6d0a06a4622a1b80a4a7
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-03-01 12:30:21 +0100

    Updated.

--- src/Components/Offer.js
+++ src/Components/Offer.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Offer() {
     return (
         <div>
-
+            <h3>Offers</h3>
+            <p>Promotions, deals, and special offers for you</p>
+            <div>
+                <img src={process.env.PUBLIC_URL + './images/offers.jpeg'} alt='Man with his kids' className='img-fluid' />
+            </div>
         </div>
     )
 }
