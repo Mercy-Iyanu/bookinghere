662e8f3fd71244e16f6a69a8a07b85c7eca15c59
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-03-01 10:24:27 +0100

    Updated.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'tailwindcss/tailwind.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './App.css';
 import App from './App';
