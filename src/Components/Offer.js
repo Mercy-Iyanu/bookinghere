9f29965118eb6d93a0f70645a0f133a0e762e796
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-04-11 07:54:08 +0100

    Updated Offer Session

--- src/Components/Offer.js
+++ src/Components/Offer.js
@@ -4,12 +4,14 @@ import Button from './Button'
 export default function Offer() {
     return (
         <div>
-            <h3>Offers</h3>
-            <p>Promotions, deals, and special offers for you</p>
+            <div className='container-fluid mt-5'>
+                <h3 className='h3 fw-bold'>Offers</h3>
+                <p>Promotions, deals, and special offers for you</p>
+            </div>
             <div className='card offer-background col-md-12'>
-                <h1>New year, new adventures</h1>
-                <p>Save 15% or more when you book and stay before April 1, 2024</p>
-                <Button />
+                <h4 className='h4 fw-bold mb-3'>New year, new adventures</h4>
+                <p className='mb-3'>Save 15% or more when you book and stay before April 1, 2024</p>
+                <button className='btn btn-primary'>Find Early 2024 Deals</button>
             </div>
 
         </div>
