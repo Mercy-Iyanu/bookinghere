6364a04c7690a049b5464c788b82773ecc0a31a0
Mercy Oyelude <mercyoyelude0@gmail.com>
2024-04-15 10:30:48 +0100

    Updated.

--- src/Components/Property.js
+++ src/Components/Property.js
@@ -7,13 +7,13 @@ export default function Property() {
                 <button className='btn btn-light' style={{ border: '1px solid grey' }}>List your property</button>
             </div>
             <hr style={{ color: 'white' }} />
-            <ul className='d-flex'>
-                <li><a href='#'>Mobile version</a></li>
-                <li><a href='#'>Your account</a></li>
-                <li><a href='#'>Make changes online to your booking</a></li>
-                <li><a href='#'>Customer Service Help</a></li>
-                <li><a href='#'>Become an affiliate</a></li>
-                <li><a href='#'>Booking.com for Booking</a></li>
+            <ul className='d-flex text-white text-1xl list-unstyled'>
+                <li><a href='#' className='text-decoration-underline fs-6'>Mobile version</a></li>
+                <li><a href='#' className='text-decoration-underline fs-6'>Your account</a></li>
+                <li><a href='#' className='text-decoration-underline fs-6'>Make changes online to your booking</a></li>
+                <li><a href='#' className='text-decoration-underline fs-6'>Customer Service Help</a></li>
+                <li><a href='#' className='text-decoration-underline fs-6'>Become an affiliate</a></li>
+                <li><a href='#' className='text-decoration-underline fs-6'>Booking.com for Booking</a></li>
             </ul>
         </div>
     )
